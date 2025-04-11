c137781122877f97049ff3734116d7f55cf45f34
ines besrour <87935896+ines-besrour@users.noreply.github.com>
2025-04-11 22:45:10 +0200

    Time: 0 ms (100%), Space: 17.9 MB (95.62%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue=deque([root])
+        levels=[]
+        i=0
+        while queue:
+            level=[]
+            for _ in range(len(queue)):
+                node=queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            if i%2:
+                level.reverse()
+            levels.append(level)
+            i+=1
+        return levels
